a9cff61ea3e9639f769f51cb5415783af575397e
vantantech <sntsoftware@yahoo.com>
2016-08-07 22:45:42 -0500

    second

--- Vidly/ViewModels/RandomMovieViewModel.cs
+++ Vidly/ViewModels/RandomMovieViewModel.cs
@@ -9,7 +9,7 @@ namespace Vidly.ViewModels
     public class RandomMovieViewModel
     {
         public Movie Movie { get; set; }
-        public List<Customer> Customers { get; set; }
+       public List<Customer> Customers { get; set; }
 
 
     }
